a148b7647187c8b0153145a65db8f114121eb9d0
Paulina Grunwald <paulina.grunwald@quasset.com>
2018-12-08 13:59:29 -0400

    Install dependencies and create test for deploying o the solidity contract

---
+++ inbox/test/Inbox.test.js
@@ -0,0 +1,28 @@
+const assert = require('assert')
+const ganache = require('ganache-cli')
+const Web3 = require('web3')
+// Create instance of web3 and connect it to local test network hosted on my machine
+const web3 = new Web3(ganache.provider())
+
+const { interface, bytecode } = require('../compile');
+
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+  // Get a list of all accounts from Ganache local test network
+  // The web3-eth is for the ethereum blockchain and smart contracts
+  accounts = await web3.eth.getAccounts();
+    
+    // Use one of those accounts to deploy the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+      .deploy({data: bytecode, arguments: ['Hi there!']})
+      .send({ from: accounts[0], gas: '1000000' });
+});
+    
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+    assert.ok(inbox.options.address);
+  });
+
+});
