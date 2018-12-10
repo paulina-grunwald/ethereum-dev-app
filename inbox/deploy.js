a41203dea697b8ab4781d1c3b701a2e1d5ab5334
Paulina Grunwald <paulina.grunwald@quasset.com>
2018-12-10 08:00:54 -0400

    Install truffle and write deployment scrip to deploy contract on Rinkeby network.

---
+++ inbox/deploy.js
@@ -0,0 +1,23 @@
+const HDWalletProvider = require('truffle-hdwallet-provider')
+const Web3 = require('web3')
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'rebuild found wife unhappy any budget prefer forest auto delay sudden play',
+  'https://rinkeby.infura.io/v3/ed69e36802b2498498263324c315c180'
+
+)
+const web3 = new Web3(provider)
+
+const deploy = async()=> {
+  accounts = await web3.eth.getAccounts();
+  console.log('Attempt to deploy on account:', accounts[0])
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({data: bytecode, arguments: ['Hi there!']})
+    .send({gas: '1000000', from: accounts[0]})
+  
+  console.log('Address of deployed contract:', result.options.address)
+}
+
+deploy()
