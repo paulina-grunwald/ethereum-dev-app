2ae49891013fd3c7188ee091a193a53efda1c4c6
Paulina Grunwald <paulina.grunwald@quasset.com>
2018-12-12 06:53:07 -0400

    Write function to choose random player and send all ether in the account balance to him.

--- lottery/contracts/Lottery.sol
+++ lottery/contracts/Lottery.sol
@@ -4,7 +4,6 @@ contract Lottery {
     address public manager;
     // create dynamic array that can only contain addresses
     address[] public players;
-    
     function Lottery() public {
         manager = msg.sender;
     }
@@ -13,4 +12,15 @@ contract Lottery {
         require(msg.value > .01 ether);
         players.push(msg.sender);
     }
+    function random() public view returns (uint){
+       return uint(keccak256(block.difficulty, now, players));
+    }
+    function pickWinner() public {
+        //get random index of the player in arrays of players
+        uint index = random() % players.length;
+        // get address of the player
+        // transfer all money from the contract and sent to selected address
+        players[index].transfer(this.balance);
+    }
+
 }
