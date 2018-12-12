3272686d0b37e037926d1f4d233a23b54873cccc
Paulina Grunwald <paulina.grunwald@quasset.com>
2018-12-12 17:33:46 -0400

    Add tests to check if the contract is deployed and if the players can enter lottery. Fix typo in deploy.js

--- lottery/deploy.js
+++ lottery/deploy.js
@@ -14,7 +14,7 @@ const deploy = async()=> {
   console.log('Attempt to deploy on account:', accounts[0])
 
   const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({data: bytecode, arguments: ['Hi there!']})
+    .deploy({data: bytecode})
     .send({gas: '1000000', from: accounts[0]})
   
   console.log('Address of deployed contract:', result.options.address)
