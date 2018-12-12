eedc483a79b668810b52f378f3f7eb34f2bedb7b
Paulina Grunwald <paulina.grunwald@quasset.com>
2018-12-12 07:35:10 -0400

    Add restricted modifiers that allows manager to run specific function. Add getPlayers function to retrieve list of players.

--- lottery/contracts/Lottery.sol
+++ lottery/contracts/Lottery.sol
@@ -15,12 +15,26 @@ contract Lottery {
     function random() public view returns (uint){
        return uint(keccak256(block.difficulty, now, players));
     }
-    function pickWinner() public {
+    function pickWinner() public restricted {
+        // only manager can call the contract
+        // require(msg.sender == manager);
         //get random index of the player in arrays of players
         uint index = random() % players.length;
         // get address of the player
         // transfer all money from the contract and sent to selected address
         players[index].transfer(this.balance);
+        // create new dynamic array of type address
+        players = new address[](0);
+    }
+    
+    modifier restricted {
+        // only manager can run function
+        require(msg.sender == manager);
+        _;
+    }
+    
+    function getPlayers() public view returns (address[]) {
+        return players;
     }
 
 }
