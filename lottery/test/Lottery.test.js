20f90d9d86cf85fc7c5b0038b0590ea93b027119
Paulina Grunwald <paulina.grunwald@quasset.com>
2018-12-13 08:00:01 -0400

    Write a test to check if it's possible to pick a winner

--- lottery/test/Lottery.test.js
+++ lottery/test/Lottery.test.js
@@ -54,4 +54,47 @@ describe('Lottery Contract', () => {
     assert.equal(2, players.length)
   })
 
+  it('players sends > 0.1 ether', async ()=> {
+    try {
+      await lottery.methods.enter().send({
+        from: accounts[0],
+        value: 200
+      })
+      assert(false)
+    } catch (err) {
+      assert(err)
+    }
+  })
+
+  it('only manager can call pickWinner', async ()=> {
+    try {
+      await lottery.methods.pickWinner().send({
+        from: accounts[1]
+      })
+      assert(false)
+    } catch (err) {
+      assert(err)
+    }
+  })
+
+  it('sends money to the winner and resets players array', async ()=> {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei('2', 'ether')
+    })
+    const initialBalance = await web3.eth.getBalance(accounts[0])
+
+    await lottery.methods.pickWinner().send({
+      from: accounts[0]
+    })
+    
+    const finalBalance = await web3.eth.getBalance(accounts[0])
+    const difference = finalBalance - initialBalance
+    assert(difference > web3.utils.toWei('1.9', 'ether'))
+
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[0]
+    })
+    assert.equal(0, players.length)
+  })
 })
