e8192e45c26d40d1fd16b3806da4d5ca237512db
Paulina Grunwald <paulina.grunwald@quasset.com>
2018-12-15 10:53:52 -0400

    Update deploy.js script with object passed to deploy function.

--- lottery/deploy.js
+++ lottery/deploy.js
@@ -1,23 +1,27 @@
-const HDWalletProvider = require('truffle-hdwallet-provider')
-const Web3 = require('web3')
+
+require('dotenv').config()
+
+
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
 
 const provider = new HDWalletProvider(
-  'rebuild found wife unhappy any budget prefer forest auto delay sudden play',
+  process.env.MNEMONIC,
   'https://rinkeby.infura.io/v3/ed69e36802b2498498263324c315c180'
+);
+const web3 = new Web3(provider);
 
-)
-const web3 = new Web3(provider)
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
 
-const deploy = async()=> {
-  accounts = await web3.eth.getAccounts();
-  console.log('Attempt to deploy on account:', accounts[0])
+  console.log('Attempting to deploy from account', accounts[0]);
 
   const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({data: bytecode})
-    .send({gas: '1000000', from: accounts[0]})
-  
-  console.log('Address of deployed contract:', result.options.address)
-}
+    .deploy({ data: '0x' + bytecode })
+    .send({ from: accounts[0], gas: '1000000'})
 
-deploy()
+  console.log(interface);
+  console.log('Contract deployed to', result.options.address);
+};
+deploy();
