ac89f66854da511297363df84eb5eb8fe6ad8a6b
Paulina Grunwald <paulina.grunwald@quasset.com>
2018-12-16 17:03:18 +0100

    Set up a form for player to enter the lottery.

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -2,22 +2,54 @@ import React, { Component } from 'react'
 import './App.css'
 import web3 from './web3'
 import lottery from './lottery'
+import { throws } from 'assert'
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { manager: '' }
-  }
-  async componentDidMount () {
-    const manager = await lottery.methods.manager().call()
-    this.setState({ manager })
+  state = {
+    manager: '',
+    players: [],
+    balance: '',
+    value: ''
   }
 
-  render () {
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call()
+    this.setState({ manager })
+    const players = await lottery.methods.getPlayers()
+    const balance = await web3.eth.getBalance.request(lottery.options.address)
+  }
+
+  onSubmit = async event => {
+    event.preventDefault()
+    const accounts = await web3.eth.getAccounts()
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, 'ether')
+    })
+  }
+  render() {
     web3.eth.getAccounts().then(console.log)
     return (
-      <div className='App'>
+      <div className="App">
         <h2>Lottery Contract</h2>
-        <p>This contract is managed by {this.state.manager}</p>
+        <p>
+          This contract is managed by <b>{this.state.manager}</b>!
+        </p>
+        <p>
+          There are currently {this.state.players.length} competing to win{' '}
+          {web3.utils.fromWei(this.state.balance, 'ether')} ether!
+        </p>
+
+        <form onSubmit={this.onSubmit}>
+          <div className="title">Do you want to win the lottery?</div>
+          <div>
+            <label>Amount of ether to enter</label>
+            <input
+              value={this.state.value}
+              onChange={event => this.setState({ value: event.target.value })}
+            />
+          </div>
+          <button>Enter lottery</button>
+        </form>
       </div>
     )
   }
