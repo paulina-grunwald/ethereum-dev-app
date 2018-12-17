f8f3fcc9c7ff4e49e7cf6de0c2ee27df66754aa4
Paulina Grunwald <paulina.grunwald@quasset.com>
2018-12-17 21:53:31 +0100

    Create request struct and use it in createRequest function.

--- campaign/contracts/Campaign.sol
+++ campaign/contracts/Campaign.sol
@@ -1,9 +1,23 @@
 pragma solidity ^0.4.17;
 
 contract Campaign {
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+    }
+
+    Request[] public request;
     address public manager;
     uint public minimumContribution;
     address[] public approvers;
+    
+    modifier restricted {
+        // only manager can run function
+        require(msg.sender == manager);
+        _;
+    }
     
     function Campain(uint minimum) public {
         // person creating the contract
@@ -14,5 +28,16 @@ contract Campaign {
     function contribute() public payable {
         require(msg.value > minimumContribution);
         approvers.push(msg.sender);
+    }
+    function createRequest(string description, uint value, address recipient) 
+        public restricted {
+            Request newRequest = Request({
+               description: description,
+               value: value,
+               recipient: recipient,
+               complete: false
+            });
+            
+            request.push(newRequest);
     }
 }
