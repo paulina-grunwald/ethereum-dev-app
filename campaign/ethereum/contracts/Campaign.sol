eedd0a633b64ac323729ef458e4e435ba2012c84
Paulina Grunwald <paulina.grunwald@quasset.com>
2018-12-31 07:11:55 +0100

    Add approveRequest function.

--- campaign/ethereum/contracts/Campaign.sol
+++ campaign/ethereum/contracts/Campaign.sol
@@ -1,17 +1,21 @@
 pragma solidity ^0.4.17;
 
 contract Campaign {
+    
     struct Request {
         string description;
         uint value;
         address recipient;
         bool complete;
+        uint approvalCount;
+        mapping(address => bool) approvals;
     }
-
-    Request[] public request;
+    
+    Request[] public requests;
     address public manager;
     uint public minimumContribution;
     mapping(address => bool) public approvers;
+    uint public approversCount;
     
     // give access to functions
     modifier restricted {
@@ -29,20 +33,32 @@ contract Campaign {
     function contribute() public payable {
         require(msg.value > minimumContribution);
         approvers[msg.sender] = true;
+        approversCount++;
     }
     
     //function creates a struct Request and adds it Request array
     function createRequest(string description, uint value, address recipient) 
-      
         public restricted {
             // create new request in memory
             Request memory newRequest = Request({
                description: description,
                value: value,
                recipient: recipient,
-               complete: false
+               complete: false,
+               approvalCount: 0
             });
             
-            request.push(newRequest);
+            requests.push(newRequest);
+    }
+    
+    function approveRequest(uint index) public {
+        Request storage request = requests[index];
+        // check if user contributed already to the campaign
+        require(approvers[msg.sender]);
+        // check if user already approved the request
+        require(!request.approvals[msg.sender]);
+
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
     }
 }
