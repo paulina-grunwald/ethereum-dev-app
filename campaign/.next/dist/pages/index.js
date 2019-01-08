c0ab318b2b049b54919fbddd1cf0640c6bbb0201
Paulina Grunwald <paulina.grunwald@quasset.com>
2019-01-08 16:18:01 +0100

    Add next and react as dependencies.

---
+++ campaign/.next/dist/pages/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = 'C:\\Personal\\ethereum-dev-app\\campaign\\pages\\index.js?entry';
+
+exports.default = function () {
+  return _react2.default.createElement('h1', {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 3
+    }
+  }, 'sadasd');
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzXFxpbmRleC5qcyJdLCJuYW1lcyI6WyJSZWFjdCJdLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQUEsQUFBTyxBQUNQOzs7Ozs7OztrQkFBZSxZQUFNLEFBQ25CO3lCQUFPLGNBQUE7O2dCQUFBO2tCQUFBO0FBQUE7QUFBQSxHQUFBLEVBQVAsQUFBTyxBQUNSO0FBRkQiLCJmaWxlIjoiaW5kZXguanM/ZW50cnkiLCJzb3VyY2VSb290IjoiQzovUGVyc29uYWwvZXRoZXJldW0tZGV2LWFwcC9jYW1wYWlnbiJ9
