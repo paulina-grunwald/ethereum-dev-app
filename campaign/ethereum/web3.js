a3aa092bbc68fdcf520a53a4a550e7ac281a0c65
Paulina Grunwald <paulina.grunwald@quasset.com>
2019-01-20 20:08:50 +0100

    Update web3 instance for server and client

--- campaign/ethereum/web3.js
+++ campaign/ethereum/web3.js
@@ -1,6 +1,18 @@
 import Web3 from 'web3'
 
-// create instance of Web3 - get access to Metamask
-const web3 = new Web3(window.web3.currentProvider)
+let web3
+
+if (typeof window !== 'undefined' && typeof window.web !== 'undefined') {
+  // We are in the browser
+  // create instance of Web3 - get access to Metamask
+  web3 = new Web3(window.web3.currentProvider)
+} else {
+  // We are on the server
+  const provider = new Web3.providers.HttpProvider(
+    'https://rinkeby.infura.io/v3/ed69e36802b2498498263324c315c180'
+
+  )
+  web3 = new Web3(provider)
+}
 
 export default web3
