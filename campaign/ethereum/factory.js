b2fe4a0acc47d005f27252e8f5f04bdf7252a1f6
Paulina Grunwald <paulina.grunwald@quasset.com>
2019-01-20 18:58:34 +0100

    Add address to instance.

--- campaign/ethereum/factory.js
+++ campaign/ethereum/factory.js
@@ -2,9 +2,9 @@ import web3 from './web3'
 
 import CampaignFactory from './build/CampaignFactory.js'
 
-const instance = new web3.eth.Contracts(
+const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  ''
+  '0x68c44895CF8e3E9aAE4De9Ff16EeD1d6a90B3671'
 )
 
 export default instance
