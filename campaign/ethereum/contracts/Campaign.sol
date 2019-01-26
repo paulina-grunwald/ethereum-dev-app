ce58d5d0a7fa9d8a4f16406c7388e3845ad579c2
Paulina Grunwald <paulina.grunwald@quasset.com>
2019-01-26 20:21:19 +0100

    update components

--- campaign/ethereum/contracts/Campaign.sol
+++ campaign/ethereum/contracts/Campaign.sol
@@ -1,22 +1,20 @@
 pragma solidity ^0.4.17;
 
 contract CampaignFactory {
-    address[] public delpoyedCampaigns;
-    
+    address[] public deployedCampaigns;
+
     function createCampaign(uint minimum) public {
-        //create new instance of Campaign
-        address newCampaign = new Campaign(minimum,msg.sender);
-        delpoyedCampaigns.push(newCampaign);
+        address newCampaign = new Campaign(minimum, msg.sender);
+        deployedCampaigns.push(newCampaign);
     }
-    
+
     function getDeployedCampaigns() public view returns (address[]) {
-        return delpoyedCampaigns;
+        return deployedCampaigns;
     }
 }
 
 contract Campaign {
-    
-   struct Request {
+    struct Request {
         string description;
         uint value;
         address recipient;
@@ -24,34 +22,30 @@ contract Campaign {
         uint approvalCount;
         mapping(address => bool) approvals;
     }
-    
+
     Request[] public requests;
     address public manager;
     uint public minimumContribution;
     mapping(address => bool) public approvers;
     uint public approversCount;
 
-    
-    // give access to functions
-    modifier restricted {
-        // only manager can run function
+    modifier restricted() {
         require(msg.sender == manager);
         _;
     }
-    
-     function Campaign(uint minimum, address creator) public {
-         // person creating the contract
+
+    function Campaign(uint minimum, address creator) public {
         manager = creator;
         minimumContribution = minimum;
     }
-    
+
     function contribute() public payable {
         require(msg.value > minimumContribution);
+
         approvers[msg.sender] = true;
         approversCount++;
     }
-    
-    //function creates a struct Request and adds it Request array
+
     function createRequest(string description, uint value, address recipient) public restricted {
         Request memory newRequest = Request({
            description: description,
@@ -63,24 +57,40 @@ contract Campaign {
 
         requests.push(newRequest);
     }
-    
+
     function approveRequest(uint index) public {
         Request storage request = requests[index];
-        // check if user contributed already to the campaign
+
         require(approvers[msg.sender]);
-        // check if user already approved the request
         require(!request.approvals[msg.sender]);
 
         request.approvals[msg.sender] = true;
         request.approvalCount++;
     }
-    
+
     function finalizeRequest(uint index) public restricted {
-        // create a variable that refers to request struct
         Request storage request = requests[index];
-        require(request.approvalCount > (approversCount/2));
+
+        require(request.approvalCount > (approversCount / 2));
         require(!request.complete);
+
         request.recipient.transfer(request.value);
         request.complete = true;
+    }
+
+    function getSummary() public view returns (
+      uint, uint, uint, uint, address
+      ) {
+        return (
+          minimumContribution,
+          this.balance,
+          requests.length,
+          approversCount,
+          manager
+        );
+    }
+
+    function getRequestsCount() public view returns (uint) {
+        return requests.length;
     }
 }
