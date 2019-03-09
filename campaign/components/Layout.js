e5b7a43bbedb4ce4f3a881312855325ac13e054d
Paulina Grunwald <paulina.grunwald@quasset.com>
2019-03-09 07:58:03 +0100

    Add new page. Move style sheet link tag to layout component.

--- campaign/components/Layout.js
+++ campaign/components/Layout.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import Header from './Header'
-
+import Head from 'next/head'
 export default function (props) {
   return (
     <div class="ui container">
+      <Head>
+        <link
+          rel='stylesheet'
+          href='//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css'
+        />
+      </Head>
       <Header />
       {props.children}
       <h1>I am a footer</h1>
---
+++ campaign/pages/campaigns/new.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Layout from '../../components/Layout'
+
+ class CampaignNew extends Component {
+  render() {
+    return (
+      <Layout>
+        <h1>
+          new campaign
+        </h1>
+      </Layout>
+    )
+  }
+}
+ export default CampaignNew
