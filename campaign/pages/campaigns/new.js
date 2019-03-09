14a22e3a4f550f4be59426113daab272d18f0744
Paulina Grunwald <paulina.grunwald@quasset.com>
2019-03-09 10:32:18 +0100

    Add onsubmit

--- campaign/pages/campaigns/new.js
+++ campaign/pages/campaigns/new.js
@@ -1,15 +1,55 @@
 import React, { Component } from 'react'
 import Layout from '../../components/Layout'
+import { Button, Form, Input } from 'semantic-ui-react'
+import './new'
+import factory from '../../ethereum/factory'
+import web3 from '../../ethereum/web3'
+class CampaignNew extends Component {
+  state = {
+     mimimumContribution: ''
+  }
 
- class CampaignNew extends Component {
+  addContribution =  (e)=> {
+    this.setState({
+      mimimumContribution: event.target.value
+    })
+  }
+
+  onSubmit = async event => {
+    event.preventDefault();
+      const accounts = await web3.eth.getAccounts();
+      await factory.methods
+        .createCampaign(this.state.minimumContribution)
+        .send({
+          from: accounts[0]
+        })
+  }
+
+  
   render() {
     return (
       <Layout>
-        <h1>
-          new campaign
-        </h1>
+        <h2>
+          New campaign
+        </h2>
+        <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
+          <Form.Field>
+            <label>Minimum Contribution</label>
+            <Input
+              type='number'
+              label="wei"
+              labelPosition="right"
+              value={this.state.minimumContribution}
+              onChange={event => this.setState({ minimumContribution: event.target.value })}
+            />
+          </Form.Field>
+          <Button primary>
+            Create!
+          </Button>
+        </Form>
       </Layout>
     )
   }
 }
- export default CampaignNew
+export default CampaignNew
+ 
